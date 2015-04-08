39588e0808e30d98590a844be4992c784779e030
Adnan <aaziz@Adnans-MacBook-Pro.local>
2015-04-07 23:12:03 -0700

    interactive button added which changes the label text upon action

--- ExampleiOSApp/ViewController.swift
+++ ExampleiOSApp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func myButton(sender: AnyObject) {
+            myLabel.text = "My Name is Adnan"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
